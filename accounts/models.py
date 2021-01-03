e29cf3de5029df3ab8b86abb96ff4a909bca9b88
Daniel Lee <13857480+Danycraft98@users.noreply.github.com>
2021-01-04 08:12:15 +0900

    Create models.py

---
+++ accounts/models.py
@@ -0,0 +1,88 @@
+from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser, PermissionsMixin
+)
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, password, email=None):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        user = self.model(
+            username=username,
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_staffuser(self, username, email, password):
+        """
+        Creates and saves a staff user with the given email and password.
+        """
+        user = self.create_user(
+            username,
+            password,
+            email=email,
+        )
+        user.staff = True
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password):
+        """
+        Creates and saves a superuser with the given email and password.
+        """
+        user = self.create_user(
+            username,
+            password,
+            email=email
+        )
+        user.staff = True
+        user.admin = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(verbose_name='email address', max_length=255, null=True)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    street = models.CharField(max_length=150)
+    zip = models.CharField(max_length=10)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=10)
+    country = models.CharField(max_length=20)
+
+    active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    staff = models.BooleanField(default=False)
+    admin = models.BooleanField(default=False)
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    def get_full_name(self):
+        return self.first_name + ' ' + self.last_name
+
+    def get_short_name(self):
+        return self.first_name
+
+    def get_last_name(self):
+        return self.last_name
+
+    def __str__(self):
+        return self.username
+
+    @property
+    def is_staff(self):
+        return self.staff
+
+    @property
+    def is_admin(self):
+        return self.admin
